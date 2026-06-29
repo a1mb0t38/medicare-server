7f7bb8b8f07ef5ffb1bcdd1d6ffca94ec11531ef
mursahaul islam noor <mursahaulnur17@gmail.com>
2026-06-29 14:30:12 +0600

    added the get prescriptions for patient api

--- index.js
+++ index.js
@@ -34,50 +34,50 @@ async function run() {
     const prescriptions = db.collection(process.env.PRESCRIPTION_COLLECTION)
 
     // all doctors
-    app.get('/doctors', async(req, res)=>{
-        try{
-          const doctors = db.collection(process.env.DOCTORS_COLLECTION)
-          const {name, specialization} = req.query;
-          const query = {};
-          
-          if(name) query.doctorName = {$regex: name, $options:'i'};
-          if(specialization) query.specialization = {$regex: specialization, $options:'i'};
+    app.get('/doctors', async (req, res) => {
+      try {
+        const doctors = db.collection(process.env.DOCTORS_COLLECTION)
+        const { name, specialization } = req.query;
+        const query = {};
 
-          const result = await doctors.find(query).toArray();
-          res.status(200).send(result)
-        }catch(error){
-          console.error(error)
-          res.status(500).json({error: 'Internal server error'})
-        }
+        if (name) query.doctorName = { $regex: name, $options: 'i' };
+        if (specialization) query.specialization = { $regex: specialization, $options: 'i' };
+
+        const result = await doctors.find(query).toArray();
+        res.status(200).send(result)
+      } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal server error' })
+      }
     })
 
     // doctor post api
-    app.post('/doctors', async(req, res)=> {
-      try{
+    app.post('/doctors', async (req, res) => {
+      try {
         const doctor = req.body;
         const existingDoctor = await doctors.findOne({
           doctorId: doctor.doctorId,
         })
-        if(existingDoctor){
+        if (existingDoctor) {
           return res.status(400).json({
             message: "Doctor already exists"
           })
         }
         const result = await doctors.insertOne(doctor)
         res.status(200).send(result)
-      }catch(error){
+      } catch (error) {
         console.error(error)
       }
     })
 
     // doctors details by id
-    app.get('/doctors/:id', async(req, res)=>{
+    app.get('/doctors/:id', async (req, res) => {
       const doctors = db.collection(process.env.DOCTORS_COLLECTION)
-      const {id} = req.params;
+      const { id } = req.params;
       const doctor = await doctors.findOne({
         _id: new ObjectId(id)
       })
-      if(!doctor){
+      if (!doctor) {
         return res.status(400).json({
           error: 'Doctor not found'
         })
@@ -86,31 +86,31 @@ async function run() {
     })
 
     // appointments api post
-    app.post('/appointments', async(req, res)=>{
-        try{
-          const appointment = req.body
+    app.post('/appointments', async (req, res) => {
+      try {
+        const appointment = req.body
         const result = await appointments.insertOne(appointment)
         res.send(result)
-        }catch(error){
-          res.status(500).send({message: "Faild to save appointment"})
-        }
+      } catch (error) {
+        res.status(500).send({ message: "Faild to save appointment" })
+      }
     })
 
 
     // get appointment for patient
-    app.get('/appointments/patient/:patientId', async(req, res)=>{
-      try{
-        const {patientId} = req.params
-        const appointment = await appointments.find({patientId}).toArray()
-        res.send(appointment); 
-      }catch(error){
+    app.get('/appointments/patient/:patientId', async (req, res) => {
+      try {
+        const { patientId } = req.params
+        const appointment = await appointments.find({ patientId }).toArray()
+        res.send(appointment);
+      } catch (error) {
         console.error(error)
       }
     })
 
     // get accepted appointment for doctor
-    app.get('/appointments/doctor/:doctorId/accepted', async(req,res)=>{
-      const {doctorId} = req.params;
+    app.get('/appointments/doctor/:doctorId/accepted', async (req, res) => {
+      const { doctorId } = req.params;
       const result = await appointments.find({
         doctorId,
         appointmentStatus: "Accepted",
@@ -119,18 +119,18 @@ async function run() {
     })
 
     // appointments status update api
-    app.patch("/appointments/:id", async(req, res)=>{
-      try{
-        const {id} = req.params;
-        const {appointmentStatus} = req.body;
+    app.patch("/appointments/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { appointmentStatus } = req.body;
 
-        if(!["Accepted", "Rejected"].includes(appointmentStatus)){
+        if (!["Accepted", "Rejected"].includes(appointmentStatus)) {
           return res.status(400).json({
             message: "invalid appointment status",
           });
         }
         const result = await appointments.updateOne(
-          {_id: new ObjectId(id)},
+          { _id: new ObjectId(id) },
           {
             $set: {
               appointmentStatus,
@@ -138,7 +138,7 @@ async function run() {
           }
         )
 
-        if(result.matchedCount === 0){
+        if (result.matchedCount === 0) {
           return res.status(400).json({
             message: "Appointment not found",
           });
@@ -149,16 +149,16 @@ async function run() {
           modifiedCount: result.modifiedCount,
         })
 
-      }catch(error){
+      } catch (error) {
         console.error(error)
       }
     })
 
     // prescriptions api
-    app.post('/prescriptions', async(req, res)=>{
-      try{
+    app.post('/prescriptions', async (req, res) => {
+      try {
         const prescription = req.body;
-        if(!prescription.doctorId || !prescription.patientId || !prescription.appointmentId || !prescription.diagnosis || !prescription.medications){
+        if (!prescription.doctorId || !prescription.patientId || !prescription.appointmentId || !prescription.diagnosis || !prescription.medications) {
           return res.status(400).json({
             message: "Missing required field",
           })
@@ -168,23 +168,38 @@ async function run() {
           ...prescription,
           createdAt: new Date(),
         })
-        
+
         res.status(200).json({
           success: true,
           message: "prescription created successfully",
         })
+
+      } catch (error) {
+        console.error(error)
+      }
+    })
+
+    // get prescription for patient
+    app.get('/prescriptions/patient/:patientId', async (req, res) => {
+      try {
+
+        const { patientId } = req.params;
+        const result = await prescriptions.find({
+          patientId
+        }).sort({ createdAt: -1 }).toArray()
+        res.send(result)
 
       }catch(error){
         console.error(error)
       }
     })
 
-      app.get('/appointments/doctor/:doctorId', async(req, res)=>{
-      try{
-        const {doctorId} = req.params
-        const appointment = await appointments.find({doctorId}).toArray()
-        res.send(appointment); 
-      }catch(error){
+    app.get('/appointments/doctor/:doctorId', async (req, res) => {
+      try {
+        const { doctorId } = req.params
+        const appointment = await appointments.find({ doctorId }).toArray()
+        res.send(appointment);
+      } catch (error) {
         console.error(error)
       }
     })
@@ -203,10 +218,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send("server is working")
+app.get('/', (req, res) => {
+  res.send("server is working")
 })
 
-app.listen(port, ()=>{
-    console.log(`server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
 })
